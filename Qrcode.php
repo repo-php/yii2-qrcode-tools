876f8604224cfefdc35e381272133e0efd56a14f
Sultonov Daler Rustamovich <daler.sultonov.00@mail.ru>
2020-08-28 14:44:06 +0500

    mew file

--- Qrcode.php
+++ Qrcode.php
@@ -5,11 +5,22 @@ namespace repoPhp\qrcode;
 /**
  * This is just an example.
  */
-class Qrcode extends \yii\base\Widget
+class Qrcode extends \yii\base\Component
 {
+    // image size in pixels
+    public $height = 200;
+
+    // image size in pixels
+    public $width = 200;
+
+    public $data;
+
+    public
     public function run()
     {
         return "Hello!";
     }
+
+
 }
 
